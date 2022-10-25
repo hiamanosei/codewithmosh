d8e61f520281e75295aea3eb0c53b7f380e4ff34
wirededdy <hiamanosei@gmail.com>
2022-10-25 11:56:23 +0000

    "Trying out"

---
+++ numbers.py
@@ -0,0 +1,14 @@
+# print(bin(7))
+# name = "kofi"
+# hi=""
+# print(bool(name))
+# print(bool(hi))
+#
+# if not hi:
+#     print("Is empty")
+# else:
+#     print("It has a value")
+
+age = 9
+message ="You are in grade 2" if age==9 else "I dont know"
+print(message)
